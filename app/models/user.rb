1f5734dfe16167d9fa1382725c2a6d6509807056
vagrant <vagrant@localhost.localdomain>
2020-11-27 09:29:58 +0000

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :fire_engines, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
+  attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # ログイン後投稿一覧画面へ
   root 'fire_engines#index'
 
-  resources :fire_engines, only: [:new, :create, :index, :show]
+  resources :fire_engines, only: [:new, :create, :index, :show] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
