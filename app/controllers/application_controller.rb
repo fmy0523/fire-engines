66aacb20baf0fd1ec7e8fea17bfce829e6802700
vagrant <vagrant@localhost.localdomain>
2020-11-29 14:13:29 +0000

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-  # ログインしていない状態でアクセスされた場合は、ログイン画面へリダイレクト
-  before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+        flash[:notice] = "Signed in successfully."
+        fire_engines_path
+  end
+
+  def after_sign_out_path_for(resource)
+        flash[:alert] = "Signed out successfully."
+        root_path
+  end
 
   protected
 
